d085e5a9e98e5598d76c9434862b6694d2421952
cabraja <cabrajamihajlo@gmail.com>
2022-09-14 21:33:49 +0200

    Splash Change

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StatusBar } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<>
+			<View style={styles.container}>
+				<View style={styles.searchContainer}>
+					<Text>Search</Text>
+				</View>
+
+				<View style={styles.mainContainer}>
+					<Text>Main content</Text>
+				</View>
+			</View>
+			<ExpoStatusBar style="auto" />
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+	},
+	searchContainer: {
+		backgroundColor: "#ACC5FF",
+		justifyContent: "center",
+		padding: 20,
+		marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+	},
+	mainContainer: {
+		flex: 1,
+		backgroundColor: "#dfe9ff",
+		justifyContent: "center",
+		alignItems: "center",
+	},
 });
